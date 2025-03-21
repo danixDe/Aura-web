bf8b9c74d4cf6ee2cee3474ad8758412127c5453
DileepKumarRambarki <rambarkidileepkumar@gmail.com>
2025-03-22 00:13:44 +0530

    googleo Auth added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { GoogleOAuthProvider } from '@react-oauth/google'
+const clientid="148388239184-5afa0ejenmi1t6gr9r444rkdpj7gn8mj.apps.googleusercontent.com";
 createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId={clientid}>
   <StrictMode>
     <App />
   </StrictMode>,
+  </GoogleOAuthProvider>
 )
