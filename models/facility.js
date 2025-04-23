279402f230bb88e3eb956269e6a360b7e0e4e62a
Bharadwaj Naidu Dasari <dbn75586@gmail.com>
2025-04-23 12:57:42 +0530

    facilities backend touch 2

--- models/facility.js
+++ models/facility.js
@@ -8,12 +8,13 @@ const facilityTable = `
    license_number VARCHAR(255) NOT NULL,
    email VARCHAR(255) UNIQUE NOT NULL,
    password VARCHAR(255) NOT NULL,
-   confirm_password VARCHAR(255) NOT NULL,
    phone VARCHAR(15) NOT NULL,
    address VARCHAR(500) NOT NULL,
    city VARCHAR(255) NOT NULL,
    state VARCHAR(255) NOT NULL,
    zip_code VARCHAR(10) NOT NULL,
+   latitude DECIMAL(10, 8) NOT NULL,
+   longitude DECIMAL(11, 8) NOT NULL,
    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
  );
 `;
